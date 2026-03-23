883f7c0be1c85311cc75963147b6bdf2af21d2ca
Jafet Alejandro Fernández <44063598+AlejandroFM180@users.noreply.github.com>
2026-03-23 12:45:15 -0600

    Actualizacion de follow mause

--- Practica 3 seguidor del mause/mause follower/src/App.jsx
+++ Practica 3 seguidor del mause/mause follower/src/App.jsx
@@ -1,14 +1,59 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { useEffect } from 'react'
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+const [enabled, setEnabled] = useState(false)
+const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', {enabled}) 
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('move', {clientX, clientY})
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      }
+    }, [enabled])
+
 
   return (
-    <h3>Mause follower</h3>
-    
+
+    <main>
+
+     <div style={{
+    position: 'absolute',
+    backgroundColor: 'rgba(29, 26, 211, 0.5)',
+    border: '1px solid #fff',
+    borderRadius: '50%',
+    opacity: 0.8,
+    pointerEvents: 'none',
+    left: -25,
+    top: -25,
+    width: 50,
+    height: 50,
+    transform: `translate(${position.x}px, ${position.y}px)`
+  }}
+  />
+
+  <button onClick={() => setEnabled(!enabled)}>
+  {enabled ? 'Activar ' : 'Desactivar '} 
+  Seguir puntero 
+  </button>
+    </main>
+
   )
 }
 
